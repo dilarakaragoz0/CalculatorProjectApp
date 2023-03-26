1e05610e4c2c80b10c7d8641f6354fa616fc9a8d
dilarakaragoz0 <dilarakaragoz71@gmail.com>
2023-03-26 19:11:50 +0300

    refactor: some minor code clean

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -13,33 +13,23 @@
             string islem = Console.ReadLine();
 
             Console.Write("ikinci sayıyı giriniz: ");
-            double sayi = double.Parse(Console.ReadLine());
+            double sayi2 = double.Parse(Console.ReadLine());
 
             double sonuc = 0;
 
             switch (islem)
             {
-                case "+":
-                    sonuc= sayi1+sayi;
-                    break;
-
-                case "-":
-                    sonuc = sayi1 - sayi;
-                    break;
-                
-                case "*":
-                    sonuc = sayi1 * sayi;
-                    break;
-                
-                case "/":
-                    sonuc = sayi1 / sayi;
-                    break;
-
-                case "%":
-                    sonuc = sayi1 % sayi;
-                    break;
+                case "+":sonuc= sayi1 + sayi2;break;
+                    
+                case "-":sonuc = sayi1 - sayi2;break;
+                    
+                case "*":sonuc = sayi1 * sayi2;break;
+                    
+                case "/":sonuc = sayi1 / sayi2;break;
+                    
+                case "%":sonuc = sayi1 % sayi2;break;
             }
-            Console.WriteLine(sayi1 + " " + islem + " " + sayi + " = "+ sonuc);
+            Console.WriteLine(sayi1 + " " + islem + " " + sayi2 + " = "+ sonuc);
         }
     }
 }
