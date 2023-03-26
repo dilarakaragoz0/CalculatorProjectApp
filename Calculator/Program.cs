0d5cdad6241d21e3d649138e6bf66978ac409852
dilarakaragoz0 <dilarakaragoz71@gmail.com>
2023-03-26 20:42:16 +0300

    test

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -20,7 +20,7 @@
             switch (islem)
             {
                 case "+":sonuc= sayi1 + sayi2;break;
-                    
+
                 case "-":sonuc = sayi1 - sayi2;break;
                     
                 case "*":sonuc = sayi1 * sayi2;break;
@@ -29,7 +29,7 @@
                     
                 case "%":sonuc = sayi1 % sayi2;break;
             }
-            Console.WriteLine(sayi1 + " " + islem + " " + sayi2 + " = "+ sonuc);
+            Console.WriteLine(sayi1 + " " + islem + " " + sayi2 + " = "+  sonuc);
         }
     }
 }
